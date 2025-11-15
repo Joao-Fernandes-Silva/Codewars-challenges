449e12848ebb20c4b20cafc22b51435c6fbd9578
Joao <joaopedrosilva33@gmail.com>
2025-11-15 11:34:03 +0000

    Fix to Population Growth

--- 7 kyu/Typescript/Pop_Growth.ts
+++ 7 kyu/Typescript/Pop_Growth.ts
@@ -1,9 +1,8 @@
 export const nbYear = (p0:number, percent:number, aug:number, p:number): number => {
     let years: number = 0;
-    let currPop: number = p0;
 
-     while (currPop < p) {
-        currPop += Math.floor(currPop * percent / 100) + aug;
+     while (p0 < p) {
+        p0 += Math.floor(p0 * percent / 100) + aug;
         years++;
     }
     return years;
